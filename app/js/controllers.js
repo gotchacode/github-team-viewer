b095ff678780e909575b84d1b30429d0ac95bb3c
vinit Kumar <vinit.kumar@changer.nl>
2014-04-23 17:20:20 +0530

    add code

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,6 +13,7 @@ function AppCtrl($scope, $http, $log, flash) {
   };
   reset();
 
+  // get members of the organization
   $scope.getMembers = function (organization) {
     reset();
     $scope.company = false;
@@ -21,34 +22,33 @@ function AppCtrl($scope, $http, $log, flash) {
       $http.get("https://api.github.com/orgs/" + organization).success(function (data) {
         angular.element('#flash-messages').css('display', 'none');
           if (data.type === 'Organization') {
-             $http.jsonp("https://api.github.com/orgs/" + organization + "/members?callback=JSON_CALLBACK")
-               .success(function (data) {
-                 $scope.members = data.data;
-                 $scope.loading = false;
-                 $scope.finding = true;
-                 $scope.company = true;
-             })
-             .error(function (data, status) {
-               console.log(data);
-               console.log(status);
-             });
-          } else {
-            flash('Please enter correct Organization name');
+            $http.jsonp("https://api.github.com/orgs/" + organization + "/members?callback=JSON_CALLBACK")
+              .success(function (data) {
+                $scope.members = data.data;
+                $scope.loading = false;
+                $scope.finding = true;
+                $scope.company = true;
+              })
+              .error(function (data, status) {
+                console.log(data);
+                console.log(status);
+              });
           }
       }).error(function (e) {
         flash('error', 'Please enter correct Organization name');
       });
   };
 
-  $scope.getUser = function (user) {
+  // get user's detail
+  $scope.getUser = function (user) { 
+    $scope.user = [];
+    $scope.repo = [];
     $scope.repos = [];
     $scope.repos = [];
     $scope.commits = [];
     $scope.current_member = null;
     $scope.current_repo = null;
 
-    $scope.user = [];
-    $scope.repo = [];
     $log.log("Getting " + user + "'s data.");
     $http.jsonp("https://api.github.com/users/" + user + "?callback=JSON_CALLBACK").success(function (data) {
       $scope.user = data.data;
@@ -56,6 +56,7 @@ function AppCtrl($scope, $http, $log, flash) {
     });
   };
 
+  // get user's repo
   $scope.getRepos = function (user) {
     $scope.repos = [];
     $scope.repos = [];
@@ -63,12 +64,14 @@ function AppCtrl($scope, $http, $log, flash) {
     $scope.current_member = null;
     $scope.current_repo = null;
 
-    $scope.repos = [];
     $log.log("Fetching projects of " + user);
-    $http.jsonp("https://api.github.com/users/" + user + "/repos?callback=JSON_CALLBACK").success(function (data) {
-      $scope.repos = data.data;
-    });
+    $http.jsonp("https://api.github.com/users/" + user + "/repos?callback=JSON_CALLBACK")
+      .success(function (data) {
+        angular.element('#flash-messages').css('display', 'none');
+        $scope.repos = data.data;
+      }).error(function () {
+        flash('error', 'User does not have any projects!');
+      });
   };
-
 }
 //MyCtrl1.$inject = [];
