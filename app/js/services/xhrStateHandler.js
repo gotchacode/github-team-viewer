73824001260a0a0c9e7df0ae85b818fabff3ed5e
vinit Kumar <vinit.kumar@changer.nl>
2014-05-03 21:01:17 +0530

    rewrite the service to make it more readable and maintainable

--- app/js/services/xhrStateHandler.js
+++ app/js/services/xhrStateHandler.js
@@ -1,95 +1,99 @@
 angular.module('teamViewerApp')
 
-.factory("XhrStateHandler", [ "$window", "ngProgress", function($window, ngProgress){
-  function Handler(){
-    
-    var instance = { }
-       ,states = [
-          { message : ""         }
-         ,{ message : "Working"  }
-         ,{ message : "Complete" }
-         ,{ message : "Success"  }
-         ,{ message : "Error"    }
-         ,{ message : "Fatal"    } 
-        ]
-       ,currentState = states[ 0 ];
-    
-    function setAPIProperties(){
-      
-      instance.message    = currentState.message;
+.factory('XhrStateHandler', [ "$window", 'ngProgress', function ($window, ngProgress) {
+  // Write Handler here
+  function Handler () {
 
+    function setAPIProperties() {
+      instance.message    = currentState.message;
       instance.isIdle     = ( currentState == states[0] );
       instance.isWorking  = ( currentState == states[1] );
       instance.isComplete = ( currentState == states[2] );
       instance.isSuccess  = ( currentState == states[3] );
       instance.isError    = ( currentState == states[4] );
-      instance.isFatal    = ( currentState == states[5] ); 
+      instance.isFatal    = ( currentState == states[5] );
     }
 
-    instance.setAllMessages = function(messages){
-       for( var i = 0; i < states.length; i++ ){
-         
-         if(!messages[i]){
-           messages[i] = "";
-         };
-         
-         states[i].message = messages[i];
-       }
+    var states = [
+      { message : ""         },
+      { message : "Working"  },
+      { message : "Complete" },
+      { message : "Success"  },
+      { message : "Error"    },
+      { message : "Fatal"    }
+    ],
+    currentState = states[0],
+
+    // going with object literal pattern
+    instance = {
+
+      setAllMessages: function(messages) {
+         for(var i = 0; i < states.length; i++) {
+
+           if(!messages[i]){
+             messages[i] = "";
+           }
+           states[i].message = messages[i];
+         }
+      },
+
+      setMessageForState: function(n,message) {
+         if(states[n]){
+           states[n]["message"] = message;
+         }
+      },
+
+      reset: function(){
+        instance.idle();
+      },
+
+      idle: function(){
+        currentState = states[ 0 ];
+        ngProgress.reset();
+        setAPIProperties();
+      },
+
+      initiate: function(){
+        currentState = states[ 1 ];
+        ngProgress.reset();
+        ngProgress.start();
+        setAPIProperties();
+      },
+
+      complete:function(){
+        currentState = states[ 2 ];
+        ngProgress.complete();
+        setAPIProperties();
+      },
+
+      success: function(){
+        currentState = states[ 3 ];
+        ngProgress.complete();
+        setAPIProperties();
+      },
+
+      error: function(){
+        currentState = states[ 4 ];
+        ngProgress.stop();
+        setAPIProperties();
+      },
+
+      fatal: function(showAlert){
+        currentState = states[ 5 ];
+        ngProgress.stop();
+        setAPIProperties();
+        if(showAlert)
+          $window
+             .alert("An unexpected error occurred while processing this request. Please refresh the page and try again.");
+      }
     };
+    return instance;
+  }
 
-     instance.setMessageForState = function(n,message){
-       if(states[n]){
-         states[n]["message"] = message;
-       };
-     };
-     instance.reset = function(){
-       instance.idle();
-     };
-     instance.idle = function(){
-       currentState = states[ 0 ];
-       ngProgress.reset();
-       setAPIProperties();
-     };
-     instance.initiate = function(){
-       currentState = states[ 1 ];
-       ngProgress.reset();
-       ngProgress.start();
-       setAPIProperties();
-     };
-
-     instance.complete = function(){
-       currentState = states[ 2 ];
-       ngProgress.complete();
-       setAPIProperties();
-     };
-
-     instance.success = function(){
-       currentState = states[ 3 ];
-       ngProgress.complete();
-       setAPIProperties();
-     };
-
-     instance.error = function(){
-       currentState = states[ 4 ];
-       ngProgress.stop();
-       setAPIProperties();
-     };
-
-     instance.fatal = function(showAlert){
-       currentState = states[ 5 ];
-       ngProgress.stop();
-       setAPIProperties();
-       if(showAlert)
-         $window
-           .alert("An unexpected error occurred while processing this request. Please refresh the page and try again.");
-     };
-     
-     return instance;  
-  };
-  Handler.getInstance = function(){
-    var ins = new Handler();
-    ins.idle();
-    return ins;
-  };
-  return Handler;
-}]);
+  Handler.getInstance = function () {
+    var instance = new Handler();
+    instance.idle();
+    return instance;
+  }
+return Handler;
+}])
